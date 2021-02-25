52f83db5a518ffe8bba038c6b1c2f7733451c38a
Agustin <agu@cursosit.com>
2021-02-25 10:59:04 -0300

    add firebase config file and delete leftover files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDQisrOYmxroh_lxWET07zummDfl0idRjc",
+    authDomain: "slack-clone-cd917.firebaseapp.com",
+    projectId: "slack-clone-cd917",
+    storageBucket: "slack-clone-cd917.appspot.com",
+    messagingSenderId: "78788287217",
+    appId: "1:78788287217:web:d78465e05c8b5d3610a750",
+    measurementId: "G-GRVL642JTJ"
+};
