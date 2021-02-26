63a2f5ea000c4fa5a57e8c7354f63084be761cdb
Agustin <agu@cursosit.com>
2021-02-26 15:02:29 -0300

    add interface with firebase in firebase.js file

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDQisrOYmxroh_lxWET07zummDfl0idRjc",
@@ -8,3 +9,10 @@ const firebaseConfig = {
     appId: "1:78788287217:web:d78465e05c8b5d3610a750",
     measurementId: "G-GRVL642JTJ"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { firebaseApp, db, auth, provider }
